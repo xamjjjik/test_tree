029b40ecc4f341d2ae4595010ebafb428e49b402
xamjjjik <averinkd@mail.ru>
2024-02-12 15:50:34 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+// Получаем данные из API
+fetch("tree.json")
+  .then((response) => response.json())
+  .then((data) => {
+    // Добавляем дерево на веб-страницу
+    const tree = document.createElement("ul");
+    document.body.appendChild(tree);
+    // Заполняем дерево из полученных данных
+    buildTree(data.services, tree);
+  });
+
+function buildTree(data, father) {
+  let servicesByHead = {};
+  let sortArray = [];
+
+  // Разделение массива по head
+  data.map((service) => {
+    if (!servicesByHead[service.head]) {
+      servicesByHead[service.head] = [service];
+      sortArray.push(service.head);
+    } else {
+      servicesByHead[service.head].push(service);
+    }
+    servicesByHead[service.head].sort((a, b) => a.sorthead - b.sorthead);
+  });
+
+  // Создание головных разделов
+  servicesByHead[null].map((item) => {
+    const li = document.createElement("li");
+    li.textContent = `${item.name}`;
+    li.id = `${item.id}`;
+    father.appendChild(li);
+  });
+
+  //Создание дополнительных разделов
+  for (let i = 1; i < sortArray.length; i++) {
+    servicesByHead[sortArray[i]].forEach((item) => {
+      const li = document.createElement("li");
+      li.textContent = `${item.name}`;
+      li.id = `${item.id}`;
+      li.className = "child";
+      const parent = document.getElementById(`${item.head}`);
+      parent.appendChild(li);
+    });
+  }
+
+  //Добавление цен
+  data.map((item) => {
+    if (item.node === 0) {
+      let el = document.getElementById(`${item.id}`);
+      el.textContent += `(${item.price})`;
+    }
+  });
+}
